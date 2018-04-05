fabf907adea2f4296ae425ceade36fea2b7a6085
Ilya Michlin <damen63@gmail.com>
2018-04-05 23:13:36 +0300

    Working modulator

---
+++ QAM.py
@@ -0,0 +1,59 @@
+'''
+This is a generic QAM mudulator. It can modulate any QAM where M=2^2k, k=1,2,3...
+
+Input:
+	data: 			input data array
+	M: 				QAM type
+	code:			modulation code. can be "gray" or "binary"
+	data_type:		input data type. can be "binary" or "numbers"
+
+Output:
+	S:				QAM modulated signal
+'''
+
+from numpy import log2, arange, sqrt, flip, concatenate, zeros, array, floor, sqrt, array, bitwise_xor
+
+def modulator(data, M, code='gray', data_type='binary'):
+	# Constants
+	sqrt_M = sqrt(M).astype(int)
+	k = log2(M).astype(int)
+
+	# Binary to Gray code constelation convertor
+	vect = array(range(sqrt_M))
+	gray_constallation = bitwise_xor(vect, floor(vect/2).astype(int))
+
+	# Gray code constelation to symbols convertor
+	vect = arange(1, sqrt(M), 2)
+	symbols = concatenate((flip(-vect, axis=0), vect)).astype(int)
+
+	# Data handling
+	data_input = data.reshape((-1,k))
+
+	# Modulation
+	if data_type == 'binary':
+		I = zeros((data_input.shape[0],))
+		Q = zeros((data_input.shape[0],))
+		for n in range(int(data_input.shape[1] / 2)):
+			I = I + data_input[:,n] * 2 ** n
+		for n in range(int(data_input.shape[1]/2),int(data_input.shape[1])):
+			Q = Q + data_input[:,n] * 2 ** (n - int(data_input.shape[1]/2))
+	elif data_type == 'numbers':
+		tmp = data / sqrt_M
+		Q = floor(tmp)
+		I = (tmp - Q) * 4
+	else:
+		return 0
+
+	I = I.astype(int)
+	Q = Q.astype(int)
+
+	if code == 'gray':
+		I = gray_constallation[I]
+		Q = gray_constallation[Q]
+
+	I = symbols[I]
+	Q = symbols[Q]
+
+	S = I + 1j * Q
+
+	return S
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# QAM Modem
+﻿# QAM modulator
+Modulation is an important part of analog communication. Analog communication is needed to transfer information where we cannot send digital signals. That includes air, cables, optic fibers etc.
+
+The purpose of a modulator is to convers one or more bits to a symbol.
+
+There are many modulation techniques but QAM is probably the most popular. QAM is both an analog and a digital modulation scheme chat combines together PSK and ASK modulations. For more reading refer to Wikipedias "Quadrature amplitude modulation" article:
+https://en.wikipedia.org/wiki/Quadrature_amplitude_modulation
+
+My goal was to create a generic QAM modulator. I ended creating a modulator that can:
+* Receive data in binary and in raw number form
+* Modulate using binary and Gray code constellation
+* Modulate any QAM where M=2^2k, k=1,2,3...
+
+## Theory
+To create QAM modulation we map the data on a constellation. For QAM-16 using Gray coding can be seen here:
+
+![QAM-16](https://upload.wikimedia.org/wikipedia/commons/d/d1/16QAM_Gray_Coded.png)
+
+As you can see, in QAM-16 each symbol represents 4 bits.
+Another important aspect of that image is the Gray coding. In Gray coding nearby symbols differ by only 1 bit. This property helps improve BER. For more reading: http://www.dsplog.com/2008/06/05/16qam-bit-error-gray-mapping/
